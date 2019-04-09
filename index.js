d48d5500a3d1cb2b78765bb53f29bc6ceb791223
pazer1 <elven404@gmail.com>
2019-04-09 13:23:46 +0900

    forth

--- index.js
+++ index.js
@@ -30,3 +30,60 @@ function carousel() {
   x[myIndex-1].style.display = "block";  
   setTimeout(carousel, 5000);
 }
+
+// 유튜브 //Magnific Popup 라이브러리
+$('.popup-youtube').magnificPopup({type:'iframe'});
+$.extend(true, $.magnificPopup.defaults, {  
+    iframe: {
+        patterns: {
+            youtube: {
+                index: 'youtube.com/', 
+                id: 'v=', 
+                src: 'https://www.youtube.com/embed/%id%?autoplay=1' 
+            }
+        }
+    }
+});
+
+// 필터 셀렉터
+filterSelection("all");
+function filterSelection(c){
+    var x,i;
+    x = document.getElementsByClassName("single-portfolio");
+    if(c=="all")c = "";
+    for(i = 0; i<x.length; i++){
+        removeClass(x[i],"show");
+        if(x[i].className.indexOf(c)>-1)addClass(x[i],"show");
+    }
+}
+
+function addClass(element, name){
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for(i = 0; i<arr2.length; i++){
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+    }
+}
+
+function removeClass(element, name){
+    var i,arr1,arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for(i = 0; i< arr2.length; i++){
+        while(arr1.indexOf(arr2[i])>-1){
+            arr1.splice(arr1.indexOf(arr2[i]),1);
+        }
+    }
+    element.className = arr1.join(" ");
+}
+
+
+var navs = document.getElementsByClassName("navli");
+for(var i = 0; i<navs.length; i++){
+    navs[i].addEventListener("click",function(){
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace("active", "");
+        this.className +=" active";
+    })
+}
