ff39d43e3b28342a516a6b3a58fa5636fac087cd
pazer1 <elven404@gmail.com>
2019-04-09 17:37:12 +0900

    fifth

--- index.js
+++ index.js
@@ -1,34 +1,106 @@
-var slideIndex = 1;
-showDivs(slideIndex);
+// 이미지 슬라이드
 
-function plusDivs(n) {
-showDivs(slideIndex += n);
+ var pos = 0;
+//number of slides
+var totalSlides = $('#slider-wrap ul li').length;
+//get the slide width
+var sliderWidth = $('#slider-wrap').width();
+
+
+$(document).ready(function(){
+	
+	
+	/*****************
+	 BUILD THE SLIDER
+	*****************/
+	//set width to be 'x' times the number of slides
+	$('#slider-wrap ul#slider').width(sliderWidth*totalSlides);
+	
+    //next slide 	
+	$('#next').click(function(){
+		slideRight();
+	});
+	
+	//previous slide
+	$('#previous').click(function(){
+		slideLeft();
+	});
+	
+	
+	
+	/*************************
+	 //*> OPTIONAL SETTINGS
+	************************/
+	//automatic slider
+	var autoSlider = setInterval(slideRight, 3000);
+	
+	//for each slide 
+	$.each($('#slider-wrap ul li'), function() { 
+
+	   //create a pagination
+	   var li = document.createElement('li');
+	   $('#pagination-wrap ul').append(li);	   
+	});
+	
+	//counter
+	countSlides();
+	
+	//pagination
+	pagination();
+	
+	//hide/show controls/btns when hover
+	//pause automatic slide when hover
+	$('#slider-wrap').hover(
+	  function(){ $(this).addClass('active'); clearInterval(autoSlider); }, 
+	  function(){ $(this).removeClass('active'); autoSlider = setInterval(slideRight, 3000); }
+	);
+	
+	
+
+});//DOCUMENT READY
+	
+
+
+/***********
+ SLIDE LEFT
+************/
+function slideLeft(){
+	pos--;
+	if(pos==-1){ pos = totalSlides-1; }
+	$('#slider-wrap ul#slider').css('left', -(sliderWidth*pos)); 	
+	
+	//*> optional
+	countSlides();
+	pagination();
 }
 
-function showDivs(n) {
-var i;
-var x = document.getElementsByClassName("mySlides");
-if (n > x.length) {slideIndex = 1}    
-if (n < 1) {slideIndex = x.length} ;
-for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-}
-x[slideIndex-1].style.display = "block";  
+
+/************
+ SLIDE RIGHT
+*************/
+function slideRight(){
+	pos++;
+	if(pos==totalSlides){ pos = 0; }
+	$('#slider-wrap ul#slider').css('left', -(sliderWidth*pos)); 
+	
+	//*> optional 
+	countSlides();
+	pagination();
 }
 
-var myIndex = 0;
-carousel();
 
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-  x[i].style.display = "none";  
-  }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 5000);
+
+	
+/************************
+ //*> OPTIONAL SETTINGS
+************************/
+function countSlides(){
+	$('#counter').html(pos+1 + ' / ' + totalSlides);
+}
+
+function pagination(){
+	$('#pagination-wrap ul li').removeClass('active');
+	$('#pagination-wrap ul li:eq('+pos+')').addClass('active');
 }
 
 // 유튜브 //Magnific Popup 라이브러리
@@ -80,10 +152,13 @@ function removeClass(element, name){
 
 
 var navs = document.getElementsByClassName("navli");
+
 for(var i = 0; i<navs.length; i++){
+  
     navs[i].addEventListener("click",function(){
-        var current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace("active", "");
-        this.className +=" active";
+     
+      var current = document.getElementsByClassName("select");
+      current[0].className = current[0].className.replace(" select", "");
+      this.className += " select";
     })
 }
