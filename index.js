93963f2de10974790b056e7e6eda2a1031457cf4
pazer1 <elven404@gmail.com>
2019-04-10 17:44:56 +0900

    compelte????

--- index.js
+++ index.js
@@ -1,124 +1,118 @@
 // 이미지 슬라이드
 
- var pos = 0;
-//number of slides
-var totalSlides = $('#slider-wrap ul li').length;
-//get the slide width
-var sliderWidth = $('#slider-wrap').width();
+/* Authored by Boaz James Otieno */
+/* full screen image slider */
+
+var arr=['./Images/sabaha.jpg','./Images/toy-story4.jpg','./Images/movieus.jpg','./Images/maxresdefault.jpg']; //an array of image sources
+var pos=0; //initializes image position in the array
+$(document).ready(function () {
+    var interval=5000; //interval for slide
+    var loaderHtml='';
+    arr.forEach(function (src) {
+        loaderHtml+='<img src="'+src+'">';
+    });
+
+    $('.load-images').html(loaderHtml);
+
+    var htm='';
+    /* initializes the small circles html*/
+    for(var i=0;i<arr.length;i++){
+        htm+='<div id="'+i+'" class="circle" onclick="circleClick('+i+')"> </div> ';
+
+    }
+
+    $('#circles').html(htm);//show small circles
+    $('#slider').html('<img src="'+arr[0]+'" class="img-slide image-animated"">');//show first image
+    $('#0').css({'background':'#fff', 'color':'#fff'});//sets the background of the first small circle to black
 
 
-$(document).ready(function(){
-	
-	
-	/*****************
-	 BUILD THE SLIDER
-	*****************/
-	//set width to be 'x' times the number of slides
-	$('#slider-wrap ul#slider').width(sliderWidth*totalSlides);
-	
-    //next slide 	
-	$('#next').click(function(){
-		slideRight();
-	});
-	
-	//previous slide
-	$('#previous').click(function(){
-		slideLeft();
-	});
-	
-	
-	
-	/*************************
-	 //*> OPTIONAL SETTINGS
-	************************/
-	//automatic slider
-	var autoSlider = setInterval(slideRight, 3000);
-	
-	//for each slide 
-	$.each($('#slider-wrap ul li'), function() { 
-		sliderWidth = $('#slider-wrap').width();
-	   //create a pagination	
-	   var li = document.createElement('li');
-	   $('#pagination-wrap ul').append(li);	   
-	});
-	
-	//counter
-	countSlides();
-	
-	//pagination
-	pagination();
-	
-	//hide/show controls/btns when hover
-	//pause automatic slide when hover
-	$('#slider-wrap').hover(
-	  function(){ $(this).addClass('active'); clearInterval(autoSlider); }, 
-	  function(){ $(this).removeClass('active'); autoSlider = setInterval(slideRight, 3000); }
-	);
-	
-	
+    /* Auto slides the images with the image sources array given as first argument and interval as second argument */
+    function autoSlide(arr,interval){
 
-});//DOCUMENT READY
-	
-
-
-/***********
- SLIDE LEFT
-************/
-function slideLeft(){
-	pos--;
-	sliderWidth = $('#slider-wrap').width();
-	alert(sliderWidth);
-	if(pos==-1){ pos = totalSlides-1; }
-	$('#slider-wrap ul#slider').css('left', -(sliderWidth*pos)); 	
-	
-	//*> optional
-	countSlides();
-	pagination();
-}
-
-
-/************
- SLIDE RIGHT
-*************/
-function slideRight(){
-	pos++;
-	sliderWidth = $('#slider-wrap').width();
-	if(pos==totalSlides){ pos = 0; }
-	$('#slider-wrap ul#slider').css('left', -(sliderWidth*pos)); 
-	
-	//*> optional 
-	countSlides();
-	pagination();
-}
-
-
-
-	
-/************************
- //*> OPTIONAL SETTINGS
-************************/
-function countSlides(){
-	$('#counter').html(pos+1 + ' / ' + totalSlides);
-}
-
-function pagination(){
-	$('#pagination-wrap ul li').removeClass('active');
-	$('#pagination-wrap ul li:eq('+pos+')').addClass('active');
-}
-
-// 유튜브 //Magnific Popup 라이브러리
-$('.popup-youtube').magnificPopup({type:'iframe'});
-$.extend(true, $.magnificPopup.defaults, {  
-    iframe: {
-        patterns: {
-            youtube: {
-                index: 'youtube.com/', 
-                id: 'v=', 
-                src: 'https://www.youtube.com/embed/%id%?autoplay=1' 
+        setInterval(function () {
+            $('.img-slide').css({'opacity':'.1 !important'});
+            pos++;
+            if(pos>arr.length-1){
+                pos=0;
             }
+
+            $('#slider').html('<img src="'+arr[pos]+'" class="img-slide img'+pos+' image-animated">');//shows image
+            $('#'+pos).css({'background':'#fff', 'color':'#fff'});//sets background-color of circle representing the current active image to black
+            $('#'+(pos-1)).css({'background':'transparent', 'color':'transparent'});//sets background-color of circle before active to white
+            if(pos==0){
+                $('#'+(arr.length-1)).css({'background':'transparent', 'color':'transparent'});
+            }
+
+        },interval);
+    }
+    /* end of function autoSlide */
+
+    autoSlide(arr,interval);//calls function autoSlide
+
+    /* displays next image */
+    function next(){
+        if(pos>arr.length-2){
+            pos=-1;
+        }
+        $('#slider').html('<img src="'+arr[pos+1]+'" class="img-slide image-animated">');//show image
+        pos++;
+
+        $('#'+pos).css({'background':'#fff', 'color':'#fff'});//sets background-color of circle representing the current active image to black
+        $('#'+(pos-1)).css({'background':'transparent', 'color':'transparent'});//sets background-color of circle before active to white
+        if(pos==0){
+            $('#'+(arr.length-1)).css({'background':'transparent', 'color':'transparent'});
         }
     }
+    /* end of function next  */
+
+    /* displays previous image */
+    function previous() {
+        if(pos<1){
+            pos=arr.length;
+        }
+        $('#slider').html('<img src="'+arr[pos-1]+'" class="img-slide image-animated">');
+        pos--;
+
+        $('#'+pos).css({'background':'#fff', 'color':'#fff'});//sets background-color of circle representing the current active image to black
+        $('#'+(pos+1)).css({'background':'transparent', 'color':'transparent'});//sets background-color of circle before active to white
+        if(pos==arr.length-1){
+            $('#0').css({'background':'transparent', 'color':'transparent'});
+        }
+    }
+    /* end of function previous */
+
+    /* onclick next */
+    $('button#next').on('click',function (e) {
+        e.preventDefault();
+        next();//call function next
+    });
+    /* end of onclick next */
+
+    /* onclick previous */
+    $('button#prev').on('click',function (e) {
+        e.preventDefault();
+        previous();//call function previous
+    });
+    /* end of onclick previous */
+
 });
+
+/* displays image represented by the small circle */
+function circleClick(position) {
+    if(position!=pos){
+        $('#slider').html('<img src="'+arr[position]+'" class="img-slide image-animated">');//show image
+
+        $('#'+position).css({'background':'#fff', 'color':'#fff'});//sets background-color of circle representing the current active image to black
+        $('#'+(pos)).css({'background':'transparent', 'color':'transparent'});//sets background-color of circle before active to white
+
+        pos=position;
+    }
+    /* end of function circleClick */
+
+
+}
+
+
 
 // 필터 셀렉터
 filterSelection("all");
